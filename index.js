a5a1df23e0e81d3a300f4cd4784da0ef796bd667
Prashant Yadav <prashant_yadav-ggn@external.mckinsey.com>
2019-03-08 17:00:03 +0530

    fixes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function unique(array) {
-  if (Array.isArray(array)) throw new TypeError("unique wants an array!");
+  if (!Array.isArray(array)) throw new TypeError("unique wants an array!");
   return [... new Set(array)];
 };
